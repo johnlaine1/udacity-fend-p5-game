220d11f8c11356cab7b652c98f66364394a4dd66
John Laine <john.laine5896@gmail.com>
2016-05-18 19:29:29 +0000

    Move enemies accross the screen

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 // Enemy class
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Reset x when enemy goes off screen
+    if (this.x >= ctx.canvas.width) {
+        this.x = -150;
+    }
+
+    this.x = this.x + (this.speed * dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,14 +31,14 @@ Player.prototype.update = function() {};
 Player.prototype.render = function() {};
 Player.prototype.handleInput = function() {};
 
+// Create the enemies.
+// Second value is y axis. 60, 145, adn 230 work pretty well to place
+// enemies is correct rows.
+var enemy1 = new Enemy(0, 60, 50),
+    enemy2 = new Enemy(200, 145, 60),
+    enemy3 = new Enemy(400, 230, 70);
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var allEnemies = [
-    new Enemy(),
-    new Enemy(),
-    new Enemy()
-    ];
+var allEnemies = [enemy1, enemy2, enemy3];
 
 // Place the player object in a variable called player
 var player = new Player();
